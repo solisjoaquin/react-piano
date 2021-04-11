10540dfb93bbb3937f8cc1b8bd1bd8ef2ddb3cb8
Joaquin <sol19008@byui.edu>
2021-04-11 01:40:57 -0300

    fixing sound of piano

--- components/Piano.js
+++ components/Piano.js
@@ -1,19 +1,27 @@
 import { useState, useReducer } from 'react'
 
 import useSound from 'use-sound';
-import c from '../sounds/mp3Notes/c5.mp3';
-import d from '../sounds/mp3Notes/d5.mp3';
-import e from '../sounds/mp3Notes/e5.mp3';
-import f from '../sounds/mp3Notes/f4.mp3';
-import g from '../sounds/mp3Notes/g4.mp3';
-import a from '../sounds/mp3Notes/a5.mp3';
-import b from '../sounds/mp3Notes/b5.mp3';
+import c from '../sounds/mp3Notes/c3.mp3';
+import d from '../sounds/mp3Notes/d3.mp3';
+import e from '../sounds/mp3Notes/e3.mp3';
+import f from '../sounds/mp3Notes/f3.mp3';
+import g from '../sounds/mp3Notes/g3.mp3';
+import a from '../sounds/mp3Notes/a4.mp3';
+import b from '../sounds/mp3Notes/b4.mp3';
+
+import c2 from '../sounds/mp3Notes/c4.mp3';
+import d2 from '../sounds/mp3Notes/d4.mp3';
+import e2 from '../sounds/mp3Notes/e4.mp3';
+import f2 from '../sounds/mp3Notes/f4.mp3';
+import g2 from '../sounds/mp3Notes/g4.mp3';
+import a2 from '../sounds/mp3Notes/a5.mp3';
+import b2 from '../sounds/mp3Notes/b5.mp3';
 import soundUrl from '../sounds/mp3Notes/guitar.mp3';
 
 
 export const Piano = () => {
 
-    const [play, { stop, isPlaying }] = useSound(soundUrl);
+    /* const [play, { stop, isPlaying }] = useSound(soundUrl); */
 
     const [note, setNote] = useState(null)
 
@@ -24,6 +32,14 @@ export const Piano = () => {
     const [playG] = useSound(g);
     const [playA] = useSound(a);
     const [playB] = useSound(b);
+
+    const [playC2] = useSound(c2);
+    const [playD2] = useSound(d2);
+    const [playE2] = useSound(e2);
+    const [playF2] = useSound(f2);
+    const [playG2] = useSound(g2);
+    const [playA2] = useSound(a2);
+    const [playB2] = useSound(b2);
 
     const handleClickC = () => {
         setNote("DO")
@@ -58,6 +74,40 @@ export const Piano = () => {
         setNote("SI")
         return playB()
     }
+
+    const handleClickC2 = () => {
+        setNote("DO")
+        return playC2()
+    }
+
+    const handleClickD2 = () => {
+        setNote("RE")
+        return playD2()
+    }
+
+    const handleClickE2 = () => {
+        setNote("MI")
+        return playE2()
+    }
+
+    const handleClickF2 = () => {
+        setNote("FA")
+        return playF2()
+    }
+
+    const handleClickG2 = () => {
+        setNote("SOL")
+        return playG2()
+    }
+
+    const handleClickA2 = () => {
+        setNote("LA")
+        return playA2()
+    }
+    const handleClickB2 = () => {
+        setNote("SI")
+        return playB2()
+    }
     return (<div>
         <h2 className="text-3xl text-white text-center font-bold leading-7 py-4 mb-8 sm:text-4xl">Nota = {note}</h2>
         <ul className="set py-5 px-4 hidden lg:block">
@@ -74,18 +124,18 @@ export const Piano = () => {
             <li onClick={handleClickA} className="white g"></li>
             <li className="black fs"></li>
             <li onClick={handleClickB} className="white f"></li>
-            <li onClick={handleClickC} className="white e"></li>
+            <li onClick={handleClickC2} className="white e"></li>
             <li className="black ds"></li>
-            <li onClick={handleClickD} className="white d"></li>
+            <li onClick={handleClickD2} className="white d"></li>
             <li className="black cs"></li>
-            <li onClick={handleClickE} className="white c"></li>
-            <li onClick={handleClickF} className="white b"></li>
+            <li onClick={handleClickE2} className="white c"></li>
+            <li onClick={handleClickF2} className="white b"></li>
             <li className="black as"></li>
-            <li onClick={handleClickG} className="white a"></li>
+            <li onClick={handleClickG2} className="white a"></li>
             <li className="black gs"></li>
-            <li onClick={handleClickA} className="white g"></li>
+            <li onClick={handleClickA2} className="white g"></li>
             <li className="black fs"></li>
-            <li onClick={handleClickB} className="white f"></li>
+            <li onClick={handleClickB2} className="white f"></li>
         </ul>
 
         <ul className="set-mini lg:hidden py-4 px-4">
